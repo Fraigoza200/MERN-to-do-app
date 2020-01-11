ee5beb952097ac91cc8f95fb4064b49c8bb6f4e5
Francisco Raigoza <raigoza.frankie@gmail.com>
2020-01-11 12:35:59 -0800

    added Form and List

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import TodoDisplay from './components/TodoDisplay'
+import TodoList from './components/TodoList'
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+
+const App = () => {
+  const [todoState, setTodoState] = useState({
+    item: '',
+    items: []
+  })
+  todoState.handleInputChange = event => {
+    setTodoState({ ...todoState,[event.target.name] : event.target.value})
+  }
+
+  todoState.handleAddTodo = event => {
+    event.preventDefault()
+    let items = JSON.parse(JSON.stringify(todoState.items))
+    items.push(todoState.item)
+    setTodoState({...todoState, items, item: ''})
+  }
+  
 }
 
 export default App;
